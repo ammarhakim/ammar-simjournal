9afc8d7689ae9d3c414164298e1c1d99c101a4e8
Ammar Hakim <a.hakim777@gmail.com>
2012-04-17 23:34:28 -0400

    Added new tests for Rk2 convergence. Next Rk3.

--- je/je12/calcConvergence.py
+++ je/je12/calcConvergence.py
@@ -6,7 +6,12 @@ def convergence(dx, err):
         od = (pylab.log(err[i]) - pylab.log(err[i-1]))/(pylab.log(dx[i]) - pylab.log(dx[i-1]))
         print od
 
+def convergenceDt(dx, err):
+    for i in range(2, dx.shape[0]):
+        od = (err[i-1]/err[i])/(dx[i-1]/dx[i])
+        print od 
+
 print "RK2, Fixed dx"
 dat = pylab.loadtxt("dt-convergence-rk2.dat")
-convergence(dat[:,0], dat[:,1])
+convergenceDt(dat[:,0], dat[:,1])
 
