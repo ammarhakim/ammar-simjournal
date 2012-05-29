3b1c19c358eff06928a38e277ac6f17cfb883f7b
Ammar Hakim <a.hakim777@gmail.com>
2012-05-29 16:03:32 -0400

    Updated more 3rd order plots.

--- sims/s139/plot-vortex-waltz.py
+++ sims/s139/plot-vortex-waltz.py
@@ -83,48 +83,6 @@ def evalSum(coeff, fields):
         res = res + coeff[i]*fields[i]
     return res
 
-def projectOnFinerGrid_f3(Xc, Yc, q):
-    dx = Xc[1]-Xc[0]
-    dy = Yc[1]-Yc[0]
-    nx = Xc.shape[0]
-    ny = Yc.shape[0]
-
-    # mesh coordinates
-    Xn = pylab.zeros((2*Xc.shape[0],), float)
-    Xn[0:nx] = Xc-0.25*dx
-    Xn[nx:] = Xc+0.25*dx
-    Xn.sort()
-
-    Yn = pylab.zeros((2*Yc.shape[0],), float)
-    Yn[0:nx] = Yc-0.25*dx
-    Yn[nx: ] = Yc+0.25*dx
-    Yn.sort()
-
-    qn = pylab.zeros((2*Xc.shape[0],2*Yc.shape[0]), float)
-
-    c0 = q[:,:,0]
-    c1 = q[:,:,1]
-    c2 = q[:,:,2]
-    c3 = q[:,:,3]
-    c4 = q[:,:,4]
-    c5 = q[:,:,5]
-    c6 = q[:,:,6]
-    c7 = q[:,:,7]
-
-    # node 0
-    qn[0:2*nx:2, 0:2*ny:2] = (9*c7)/16.0+(3*c6)/16.0+(3*c5)/16.0+(9*c4)/16.0-(3*c3)/16.0-c2/8.0-(3*c1)/16.0
-
-    # node 1
-    qn[1:2*nx:2, 0:2*ny:2] = (3*c7)/16.0+(3*c6)/16.0+(9*c5)/16.0+(9*c4)/16.0-c3/8.0-(3*c2)/16.0-(3*c0)/16.0
-
-    # node 2
-    qn[1:2*nx:2, 1:2*ny:2] = (3*c7)/16.0+(9*c6)/16.0+(9*c5)/16.0+(3*c4)/16.0-(3*c3)/16.0-(3*c1)/16.0-c0/8.0
-
-    # node 3
-    qn[0:2*nx:2, 1:2*ny:2] = (9*c7)/16.0+(9*c6)/16.0+(3*c5)/16.0+(3*c4)/16.0-(3*c2)/16.0-c1/8.0-(3*c0)/16.0
-
-    return Xn, Yn, qn
-
 def projectOnFinerGrid_f34(Xc, Yc, q):
     dx = Xc[1]-Xc[0]
     dy = Yc[1]-Yc[0]
