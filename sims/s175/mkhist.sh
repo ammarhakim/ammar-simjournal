54fa32e2a2da90f6d5895347fd726e5edc143f87
Ammar <11265732+ammarhakim@users.noreply.github.com>
2012-07-27 13:42:25 -0400

    More input files for ion acoustic wave problem.

---
+++ sims/s175/mkhist.sh
@@ -0,0 +1,1 @@
+$HOME/research/gkeyll-project/gkeyllall/gkeyll/scripts/gethist.py s175-ion-sound_fieldEnergy DataStruct > fieldEnergy
---
+++ sims/s176/mkhist.sh
@@ -0,0 +1,1 @@
+$HOME/research/gkeyll-project/gkeyllall/gkeyll/scripts/gethist.py s176-ion-sound_fieldEnergy DataStruct > fieldEnergy
