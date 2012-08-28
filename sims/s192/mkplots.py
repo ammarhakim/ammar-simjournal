d40ed20d3e416e95b07afc7ab0f2e61f525fbdbb
Ammar Hakim <a.hakim777@gmail.com>
2012-08-28 11:44:09 -0400

    Added new script to test energy/momentum conservation

---
+++ sims/s192/mkplots.py
@@ -0,0 +1,26 @@
+from pylab import *
+
+figure(1)
+# energy
+fe = loadtxt("fieldEnergy")
+pe = loadtxt("ptclEnergy")
+te = fe[:,1] + pe[:,1]
+plot(fe[:,0], te)
+savefig("s192-total-energy.png")
+print "Relative change in energy", (te[-1]-te[0])/te[0]
+
+figure(2)
+# momentum
+pm = loadtxt("ptclMomentum")
+ptclMom = pm[:,1]
+plot(pm[:,0], ptclMom)
+savefig("s192-total-momentum.png")
+print "Relative change in momentum", (ptclMom[-1]-ptclMom[0])/ptclMom[0]
+
+figure(3)
+# num density in cell
+pm = loadtxt("numDensityInCell")
+plot(pm[:,0], pm[:,1])
+savefig("s192-numDensityInCell.png")
+
+show()
