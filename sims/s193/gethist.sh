b8df8983606e5ee77c59f3c734c6abf35715229e
Ammar Hakim <a.hakim777@gmail.com>
2012-08-28 15:18:33 -0400

    More input files for convergece of energy errors with DG3 scheme.

---
+++ sims/s193/gethist.sh
@@ -0,0 +1,5 @@
+python /Users/ahakim/research/gkeyll-project/gkeyllall/gkeyll/scripts/gethist.py s193-landau-damping-vp_totalPtcl DataStruct > totalPtcl
+python /Users/ahakim/research/gkeyll-project/gkeyllall/gkeyll/scripts/gethist.py s193-landau-damping-vp_fieldEnergy DataStruct > fieldEnergy
+python /Users/ahakim/research/gkeyll-project/gkeyllall/gkeyll/scripts/gethist.py s193-landau-damping-vp_totalPtclEnergy DataStruct > ptclEnergy
+python /Users/ahakim/research/gkeyll-project/gkeyllall/gkeyll/scripts/gethist.py s193-landau-damping-vp_totalPtclMomentum DataStruct > ptclMomentum
+python /Users/ahakim/research/gkeyll-project/gkeyllall/gkeyll/scripts/gethist.py s193-landau-damping-vp_numDensInCell DataStruct > numDensityInCell
