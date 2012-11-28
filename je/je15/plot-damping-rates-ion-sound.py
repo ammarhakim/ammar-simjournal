c0ed65cda7698f2f2a5b361c9eb43de4fd134fa2
Ammar Hakim <a.hakim777@gmail.com>
2012-11-28 16:24:57 -0500

    Updated figure with exact damping results for ion-acoustic waves.

--- je/je15/plot-damping-rates-ion-sound.py
+++ je/je15/plot-damping-rates-ion-sound.py
@@ -7,9 +7,14 @@ import math
 figure(1)
 rc('text', usetex=True)
 dat = loadtxt('damping-rates-ion-sound.txt')
-ndr = dat[:,1]/(0.5*sqrt(1/dat[:,0]))
-semilogy(dat[:,0], ndr, '-ro')
+semilogy(dat[:,0], 2*dat[:,1], 'ko')
 xlabel(r'$T = T_i/T_e$')
-ylabel('Normalized Damping Rate (\gamma/c_ek)')
+
+datEx = loadtxt('exact-ld-damping-rates-ion-acoustic.txt')
+semilogy(datEx[:,0], -datEx[:,1], 'm-', label='Exact')
+
+ylabel('Normalized Damping Rate ($\gamma/\sqrt{2} v_t k$)')
+pylab.axis('tight')
 savefig('damping-rates-ion-sound.png')
+show()
 close()
