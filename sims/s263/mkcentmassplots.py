9f4f5ad503c3067df8e984f9bd40848f4ff9b08c
Ammar <11265732+ammarhakim@users.noreply.github.com>
2013-07-19 12:05:53 -0400

    Added new simulation on a larger grid. Results change a lot.

--- sims/s263/mkcentmassplots.py
+++ sims/s263/mkcentmassplots.py
@@ -2,15 +2,13 @@ from pylab import *
 import numpy
 
 # load data
-com_260 = loadtxt("s260-blobs_centerOfMass.txt")
-com_261 = loadtxt("../s261/s261-blobs_centerOfMass.txt")
-com_262 = loadtxt("../s262/s262-blobs_centerOfMass.txt")
+com_260 = loadtxt("../s260/s260-blobs_centerOfMass.txt")
+com_263 = loadtxt("s263-blobs_centerOfMass.txt")
 
 # plot COM data
 figure(1)
-plot(com_260[:,0], com_260[:,1]-5, label='Ra = 1e4')
-plot(com_261[:,0], com_261[:,1]-5, label='Ra = 1e6')
-plot(com_262[:,0], com_262[:,1]-5, label='Ra = 1e8')
+plot(com_260[:,0], com_260[:,1]-5, label='260')
+plot(com_263[:,0], com_263[:,1]-10, label='263')
 title('Center-of-Mass (radial coordinate)')
 xlabel('Time [s]')
 ylabel('X location')
@@ -32,14 +30,12 @@ def calcVel(T, xloc):
 skip = 13
 # compute velocity
 tm_260, v_260 = calcVel(com_260[0:-1:skip,0], com_260[0:-1:skip,1])
-tm_261, v_261 = calcVel(com_261[0:-1:skip,0], com_261[0:-1:skip,1])
-tm_262, v_262 = calcVel(com_262[0:-1:skip,0], com_262[0:-1:skip,1])
+tm_263, v_263 = calcVel(com_263[0:-1:skip,0], com_263[0:-1:skip,1])
 
 # plot COM velocity dataZ
 figure(2)
-plot(tm_260, v_260, label='Ra = 1e4')
-plot(tm_261, v_261, label='Ra = 1e6')
-plot(tm_262, v_262, label='Ra = 1e8')
+plot(tm_260, v_260, label='260')
+plot(tm_263, v_263, label='263')
 title('Radial velocity')
 xlabel('Time [s]')
 ylabel('Radial velocity')
