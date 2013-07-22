71a82a6b7a3a4d362ae4865c169237a2be4d934f
Ammar Hakim <a.hakim777@gmail.com>
2013-07-21 22:00:38 -0400

    Added new python plotting scripts. Fixing for the drift matters little.

--- sims/s265/mkcentmassplots.py
+++ sims/s265/mkcentmassplots.py
@@ -2,21 +2,18 @@ from pylab import *
 import numpy
 
 # load data
-com_266 = loadtxt("../s266/s266-blobs_centerOfMass.txt")
-arcOn_tm = loadtxt('craig-time.txt')
-arcOn_xl = loadtxt('craig-com.txt')
-arcOn_vl = loadtxt('craig-com-vel.txt')
+com_263 = loadtxt("../s263/s263-blobs_centerOfMass.txt")
+com_265 = loadtxt("../s265/s265-blobs_centerOfMass.txt")
 
 # plot COM data
 figure(1)
-plot(com_266[:,0], com_266[:,1]-10, label='Gkeyll')
-plot(arcOn_tm, arcOn_xl, label='arcOn')
-
+plot(com_263[:,0], com_263[:,1]-10, label='263')
+plot(com_265[:,0], com_265[:,1]-10, label='265')
 title('Center-of-Mass (radial coordinate)')
 xlabel('Time [s]')
 ylabel('X location')
 legend(loc='upper left')
-savefig('arcOn-gkeyll-blob-xloc.png')
+savefig('blob-xloc.png')
 
 def calcVel(T, xloc):
     nt = T.shape[0]-1
@@ -30,18 +27,19 @@ def calcVel(T, xloc):
     return tm, vx
 
 # this skip is needed to smooth out some of the spiky data
-skip = 3
+skip = 13
 # compute velocity
-tm_266, v_266 = calcVel(com_266[0:-1:skip,0], com_266[0:-1:skip,1])
+tm_263, v_263 = calcVel(com_263[0:-1:skip,0], com_263[0:-1:skip,1])
+tm_265, v_265 = calcVel(com_265[0:-1:skip,0], com_265[0:-1:skip,1])
 
 # plot COM velocity dataZ
 figure(2)
-plot(tm_266, v_266, label='Gkeyll')
-plot(arcOn_tm[:-1], arcOn_vl, label='arcOn')
+plot(tm_263, v_263, label='263')
+plot(tm_265, v_265, label='265')
 title('Radial velocity')
 xlabel('Time [s]')
 ylabel('Radial velocity')
 legend(loc='upper right')
-savefig('arcOn-gkeyll-blob-vrad.png')
+savefig('blob-vrad.png')
 
 show()
