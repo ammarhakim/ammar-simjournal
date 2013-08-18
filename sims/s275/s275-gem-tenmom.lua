f0dbe798a5d30f38041f87f66ec0ad860b39934d
Ammar <11265732+ammarhakim@users.noreply.github.com>
2013-08-18 19:49:59 -0400

    Correct tEnd.

--- sims/s275/s275-gem-tenmom.lua
+++ sims/s275/s275-gem-tenmom.lua
@@ -529,9 +529,9 @@ writeFrame(0, 0.0)
 dtSuggested = 1.0 -- initial time-step to use (this will be discarded and adjusted to CFL value)
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 200.0
+tEnd = 60.0/wci
 
-nFrames = 20
+nFrames = 60
 tFrame = (tEnd-tStart)/nFrames -- time between frames
 
 tCurr = tStart
