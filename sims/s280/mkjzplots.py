b477ffb7c6339c19c588740fd671c527d68bd3d5
Ammar <11265732+ammarhakim@users.noreply.github.com>
2013-09-05 16:08:09 -0400

    Added new simulations. Also added a new TW notebook with results of reconnection.

--- sims/s280/mkjzplots.py
+++ sims/s280/mkjzplots.py
@@ -2,17 +2,22 @@ import numpy
 from pylab import *
 import tables
 
+rc('text', usetex=True)
+
 LX = 50.0
 LY = 25.0
 B0 = 1/15.0
 n0 = 1.0
 mu0 = 1.0
+elcCharge = -1.0
+ionCharge = 1.0
 ionMass = 1.0
 elcMass = ionMass/25
 va = B0/sqrt(mu0*elcMass*n0)
+ionCycl = ionCharge*B0/ionMass
 
 fh = tables.openFile("s280-gemguide-5m_q_30.h5")
-tm = fh.root.timeData._v_attrs['vsTime']
+tm = fh.root.timeData._v_attrs['vsTime']*ionCycl
 q = fh.root.StructGridField
 nx, ny = q.shape[0], q.shape[1]
 
@@ -32,12 +37,12 @@ xten = maxx-minx
 figure(1)
 subplot(2,1,2)
 pcolormesh(XX, YY, transpose(jz))
-axis('image')
+axis('tight')
 plot([minx,minx], [-LY/2,LY/2], '--w')
 plot([maxx,maxx], [-LY/2,LY/2], '--w')
 
-title('Electron out-of-plane current, t=%g. Extension=%g' % (tm,xten) )
-colorbar()
+title('Electron out-of-plane current, $t\Omega_{ci}$=%g. Extension=%g' % (tm,xten) )
+#colorbar()
 
 #figure(2)
 subplot(2,1,1)
@@ -46,7 +51,7 @@ axis('tight')
 yl = gca().get_ylim()
 plot([minx,minx], yl, '--k')
 plot([maxx,maxx], yl, '--k')
-title('5M Normalized electron out-flow velocity at t=%g' % tm)
+title('5M Normalized electron out-flow velocity at $t\Omega_{ci}$=%g' % tm)
 savefig('s280_extension.png')
 
 show()
