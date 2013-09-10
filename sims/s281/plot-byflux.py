ba81771ddec7eac9913ae422acb74d9ed75dd6ed
Ammar Hakim <a.hakim777@gmail.com>
2013-09-09 21:19:46 -0400

    Updated plotting scripts

--- sims/s281/plot-byflux.py
+++ sims/s281/plot-byflux.py
@@ -2,6 +2,18 @@ import tables
 import numpy
 from pylab import *
 
+LX = 50.0
+LY = 25.0
+B0 = 1/15.0
+n0 = 1.0
+mu0 = 1.0
+elcCharge = -1.0
+ionCharge = 1.0
+ionMass = 1.0
+elcMass = ionMass/25
+va = B0/sqrt(mu0*elcMass*n0)
+ionCycl = ionCharge*B0/ionMass
+
 def calcDeriv(T, func):
     nt = T.shape[0]-1
     tm = numpy.zeros((nt,), numpy.float)
@@ -19,7 +31,7 @@ byFlux = dat[:,1]
 tm, diffByFlux = calcDeriv(T, byFlux)
 
 figure(1)
-plot(tm, diffByFlux)
+plot(tm*ionCycl, diffByFlux)
 title('Reconnected flux rate')
 xlabel('T')
 ylabel('d(flux)/dt')
