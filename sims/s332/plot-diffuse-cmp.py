4c9dda1d80187f120a820dbb77b82ad33957e682
Ammar Hakim <a.hakim777@gmail.com>
2013-12-01 21:14:38 -0500

    Added new simulations.

---
+++ sims/s332/plot-diffuse-cmp.py
@@ -0,0 +1,88 @@
+import tables
+import pylab
+import math
+import numpy
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+
+# customization for figure
+rcParams['lines.linewidth']            = 1
+rcParams['font.size']                  = 18
+#rcParams['xtick.major.size']           = 8 # default is 4
+#rcParams['xtick.major.width']          = 3 # default is 0.5
+#rcParams['ytick.major.size']           = 8 # default is 4
+#rcParams['ytick.major.width']          = 3 # default is 0.5
+rcParams['figure.facecolor']           = 'white'
+#rcParams['figure.subplot.bottom']      = 0.125
+#rcParams['figure.subplot.right']       = 0.85 # keep labels/ticks of colobar in figure
+rcParams['image.interpolation']        = 'none'
+rcParams['image.origin']               = 'lower'
+rcParams['contour.negative_linestyle'] = 'solid'
+#rcParams['savefig.bbox']               = 'tight'
+
+def plotLines(X, q0, q1, color):
+    for i in range(X.shape[0]-1):
+        ql = q0[i] - q1[i]
+        qr = q0[i] + q1[i]
+        pylab.plot([X[i], X[i+1]], [ql, qr], color)
+
+def calcDeriv(T, val):
+    dVal = numpy.zeros((T.shape[0]-1,), numpy.float)
+    tVal = numpy.zeros((T.shape[0]-1,), numpy.float) 
+    for i in range(T.shape[0]-1):
+        tVal[i] = 0.5*(T[i]+T[i+1])
+        vAv = 0.5*(val[i+1]+val[i])
+        dVal[i] = (val[i+1]-val[i])/(T[i+1]-T[i])
+    return tVal, dVal
+
+def exactSol(X, tend):
+    return pylab.exp(-alpha*tend)*numpy.sin(X)
+
+def exactSolSS(X):
+    return numpy.sin(X)
+
+baseList = ["../s329/s329-modal-dg-diffuse", "../s330/s330-modal-dg-diffuse", 
+            "../s331/s331-modal-dg-diffuse", "../s332/s332-modal-dg-diffuse"]
+titleStr = ["LDG-L", "LDG-R", "LDG-S", "RDG"]
+count = 0
+
+pylab.figure(1)
+for baseName in baseList:
+    pylab.subplot(2, 2, count+1)
+
+    alpha = 1.0
+    tend = 1.0
+
+    fh = tables.openFile(baseName+"_q_0.h5")
+    q0 = fh.root.StructGridField
+
+    grid = fh.root.StructGrid
+    lower = grid._v_attrs.vsLowerBounds
+    upper = grid._v_attrs.vsUpperBounds
+    cells = grid._v_attrs.vsNumCells
+
+    nx = cells[0]
+    dx = (upper[0]-lower[0])/cells[0]
+    X = pylab.linspace(lower[0], upper[0], nx+1)
+
+    fh = tables.openFile(baseName+"_q_1.h5")
+    q = fh.root.StructGridField
+
+    plotLines(X, q[:,0], q[:,1], '-k')
+
+    fh = tables.openFile(baseName+"_src.h5")
+    src = fh.root.StructGridField
+    plotLines(X, src[:,0], src[:,1], '--og')
+
+    Xhr = pylab.linspace(lower[0], upper[0], 1000)
+    qExact = exactSolSS(Xhr)
+    pylab.plot(Xhr, qExact, '-m', linewidth=0.5)
+    #pylab.title('Steady-state solution for %s scheme' % titleStr[count] )
+    #pylab.xlabel('X')
+    #pylab.ylabel('T')
+    pylab.axis('tight')
+    count = count + 1
+
+pylab.savefig('s329-s330-s331-s332-dg-diffuse.png', dpi=300)
+pylab.show()
+
---
+++ sims/s332/s332-modal-dg-diffuse.lua
@@ -0,0 +1,227 @@
+-- Input file for Poisson bracket operator
+
+-- polynomial order
+polyOrder = 1
+
+-- cfl number to use
+cfl = 0.1/(2*polyOrder+1)
+
+LX = 2*Lucee.Pi -- domain length
+NX = 8 -- number of cells
+dx = LX/NX -- cell size
+
+-- grid on which equations are to be solved
+grid = Grid.RectCart1D {
+   lower = {0},
+   upper = {LX},
+   cells = {NX},
+}
+
+-- solution
+q = DataStruct.Field1D {
+   onGrid = grid,
+   numComponents = polyOrder+1,
+   ghost = {2, 2},
+}
+-- for RK time-stepping
+q1 = DataStruct.Field1D {
+   onGrid = grid,
+   numComponents = polyOrder+1,
+   ghost = {2, 2},
+}
+-- updated solution
+qNew = DataStruct.Field1D {
+   onGrid = grid,
+   numComponents = polyOrder+1,
+   ghost = {2, 2},
+}
+-- heat source
+src = DataStruct.Field1D {
+   onGrid = grid,
+   numComponents = polyOrder+1,
+   ghost = {2, 2},
+}
+
+-- duplicate for use in time-stepping
+qNewDup = qNew:duplicate()
+
+-- updater to apply initial conditions
+initField = Updater.ProjectOnBasis1D {
+   onGrid = grid,
+   numBasis = polyOrder+1,
+   project = function (x,y,z,t)
+		return 0.0
+	     end
+}
+initField:setOut( {q} )
+-- initialize
+initField:advance(0.0) -- time is irrelevant
+
+-- updater to apply initial conditions
+initSrc = Updater.ProjectOnBasis1D {
+   onGrid = grid,
+   numBasis = polyOrder+1,
+   project = function (x,y,z,t)
+		return math.sin(x)
+	      end
+}
+initSrc:setOut( {src} )
+-- initialize
+---initSrc:advance(0.0) -- time is irrelevant
+
+-- function to initialize source with an exact projection
+function initSrcExact(x,y,z)
+   local xj = x
+   local f0 = (math.cos((2*xj-dx)/2.0)-math.cos((2*xj+dx)/2.0))/dx
+   local f1 = ((2*math.sin((2*xj+dx)/2)-dx*math.cos((2*xj+dx)/2)-2*math.sin((2*xj-dx)/2)-dx*math.cos((2*xj-dx)/2))/dx)*3.0/dx
+   return f0, f1
+end
+src:set(initSrcExact)
+src:write("src.h5")
+
+-- updater to solve diffusion equation
+diffSolver = Updater.DGDiffusion1D { 
+   onGrid = grid,
+   -- polynomial order
+   polyOrder = 1,
+   -- scheme
+   scheme = "RDG",
+   -- diffusion coefficent
+   diffusionCoeff = 1.0,
+   -- CFL number
+   cfl = cfl,
+}
+
+-- L2 norm of solution (energy)
+l2Norm = DataStruct.DynVector {
+   -- number of components in diagnostic
+   numComponents = 1,
+}
+
+l2NormCalc = Updater.ModalL2Norm {
+   onGrid = grid,
+   -- number of basis function
+   numBasis = polyOrder+1,
+}
+l2NormCalc:setIn( {q} )
+l2NormCalc:setOut( {l2Norm} )
+
+-- compute L2 norm of initial conditions
+l2NormCalc:advance(0.0)
+
+-- apply boundary conditions
+function applyBc(fld)
+   fld:applyPeriodicBc(0)
+end
+
+applyBc(q)
+qNew:copy(q)
+
+-- write initial conditions
+q:write("q_0.h5")
+
+-- solve advection equation
+function solveDiffusion(curr, dt, qIn, qOut)
+   diffSolver:setCurrTime(curr)
+   diffSolver:setIn( {qIn} )
+   diffSolver:setOut( {qOut} )
+   local myS, myDt = diffSolver:advance(curr+dt)
+   -- accumulate source
+   qOut:accumulate(dt, src)
+
+   return myS, myDt
+end
+
+function calcDiagnostics(curr, dt)
+   l2NormCalc:setCurrTime(curr)
+   l2NormCalc:advance(curr+dt)
+end
+
+-- function to take a time-step using SSP-RK3 time-stepping scheme
+function rk3(tCurr, myDt)
+   -- RK stage 1
+   local myStatus, myDtSuggested = solveDiffusion(tCurr, myDt, q, q1)
+   if (myStatus == false) then
+      return myStatus, myDtSuggested
+   end
+   applyBc(q1)
+
+   -- RK stage 2
+   local myStatus, myDtSuggested = solveDiffusion(tCurr, myDt, q1, qNew)
+   if (myStatus == false) then
+      return myStatus, myDtSuggested
+   end
+   q1:combine(3.0/4.0, q, 1.0/4.0, qNew)
+   applyBc(q1)
+
+   -- RK stage 3
+   local myStatus, myDtSuggested = solveDiffusion(tCurr, myDt, q1, qNew)
+   if (myStatus == false) then
+      return myStatus, myDtSuggested
+   end
+
+   q1:combine(1.0/3.0, q, 2.0/3.0, qNew)
+   applyBc(q1)
+   q:copy(q1)
+
+   return myStatus, myDtSuggested
+end
+
+-- function to advance solution from tStart to tEnd
+function advanceFrame(tStart, tEnd, initDt)
+   local step = 1
+   local tCurr = tStart
+   local myDt = initDt
+   local status, dtSuggested
+
+   while tCurr<=tEnd do
+      qNewDup:copy(qNew)
+      if (tCurr+myDt > tEnd) then -- adjust to tEnd exactly
+	 myDt = tEnd-tCurr
+      end
+
+      print (string.format("Taking step %d at time %g with dt %g", step, tCurr, myDt))
+      status, dtSuggested = rk3(tCurr, myDt)
+
+      if (status == false) then
+	 print (string.format("** Time step %g too large! Will retake with dt %g", myDt, dtSuggested))
+	 qNew:copy(qNewDup)
+	 myDt = dtSuggested
+      else
+	 calcDiagnostics(tCurr, myDt)
+
+	 tCurr = tCurr + myDt
+	 myDt = dtSuggested
+	 step = step + 1
+	 if (tCurr >= tEnd) then
+	    break
+	 end
+      end
+   end
+
+   return dtSuggested
+end
+
+-- write data to H5 file
+function writeFields(frame)
+   q:write( string.format("q_%d.h5", frame) )
+   l2Norm:write( string.format("l2Norm_%d.h5", frame) )
+end
+
+-- parameters to control time-stepping
+tStart = 0.0
+tEnd = 10.0
+dtSuggested = 0.1*tEnd -- initial time-step to use (will be adjusted)
+nFrames = 1
+tFrame = (tEnd-tStart)/nFrames -- time between frames
+
+tCurr = tStart
+for frame = 1, nFrames do
+
+   Lucee.logInfo (string.format("-- Advancing solution from %g to %g", tCurr, tCurr+tFrame))
+   dtSuggested = advanceFrame(tCurr, tCurr+tFrame, dtSuggested)
+   writeFields(frame)
+   tCurr = tCurr+tFrame
+   Lucee.logInfo ("")
+end
+
