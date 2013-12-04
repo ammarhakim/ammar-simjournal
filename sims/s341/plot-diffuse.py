a4e1b8c6e0c85b257953bacdd3b60f10d0efd227
Ammar Hakim <a.hakim777@gmail.com>
2013-12-04 15:07:40 -0500

    Updated plots

--- sims/s341/plot-diffuse.py
+++ sims/s341/plot-diffuse.py
@@ -41,14 +41,12 @@ def exactSol(X, tend):
 def exactSolSS(X):
     return numpy.sin(X)
 
-baseList = ["../s329/s329-modal-dg-diffuse", "../s330/s330-modal-dg-diffuse", 
-            "../s331/s331-modal-dg-diffuse", "../s332/s332-modal-dg-diffuse"]
-titleStr = ["LDG-L", "LDG-R", "LDG-S", "RDG"]
+baseList = ["../s337/s337-modal-dg-diffuse", "../s341/s341-modal-dg-diffuse"]
 count = 0
 
 pylab.figure(1)
 for baseName in baseList:
-    pylab.subplot(2, 2, count+1)
+    pylab.subplot(2, 1, count+1)
 
     alpha = 1.0
     tend = 1.0
@@ -65,17 +63,13 @@ for baseName in baseList:
     dx = (upper[0]-lower[0])/cells[0]
     X = pylab.linspace(lower[0], upper[0], nx+1)
 
-    fh = tables.openFile(baseName+"_q_20.h5")
+    fh = tables.openFile(baseName+"_q_1.h5")
     q = fh.root.StructGridField
 
     plotLines(X, q[:,0], q[:,1], '-k')
 
-    fh = tables.openFile(baseName+"_src.h5")
-    src = fh.root.StructGridField
-    plotLines(X, src[:,0], src[:,1], '--og')
-
     Xhr = pylab.linspace(lower[0], upper[0], 1000)
-    qExact = exactSolSS(Xhr)
+    qExact = exactSol(Xhr, 3.0/20.0)
     pylab.plot(Xhr, qExact, '-m', linewidth=0.5)
     #pylab.title('Steady-state solution for %s scheme' % titleStr[count] )
     #pylab.xlabel('X')
@@ -83,6 +77,7 @@ for baseName in baseList:
     pylab.axis('tight')
     count = count + 1
 
-pylab.savefig('s329-s330-s331-s332-dg-diffuse.pdf')
+pylab.savefig('s329-s330-s331-s332-dg-diffuse.png', dpi=300)
 pylab.show()
+
 
