5b6b9ea59e0da0ba8229848e750e08301801291d
Ammar Hakim <a.hakim777@gmail.com>
2014-01-24 13:26:27 -0500

    Added new file for a plot

---
+++ je/je21/plot-beta-10-kp-scan-iaea.py
@@ -0,0 +1,42 @@
+from pylab import *
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import pylab
+import numpy
+import math
+
+# customization for figure
+rcParams['lines.linewidth']            = 3
+rcParams['font.size']                  = 18
+#rcParams['xtick.major.size']           = 8 # default is 4
+#rcParams['xtick.major.width']          = 3 # default is 0.5
+#rcParams['ytick.major.size']           = 8 # default is 4
+#rcParams['ytick.major.width']          = 3 # default is 0.5
+rcParams['figure.facecolor']           = 'white'
+#rcParams['figure.subplot.bottom']      = 0.125
+#rcParams['figure.subplot.right']       = 0.85 # keep labels/ticks of colobar in figure
+rcParams['image.interpolation']        = 'none'
+rcParams['image.origin']               = 'lower'
+rcParams['contour.negative_linestyle'] = 'solid'
+#rcParams['savefig.bbox']               = 'tight'
+
+fig, ax1 = subplots()
+
+# plot frequencies
+dat = loadtxt('beta-10-kp-scan')
+freq = 2*pi/(2*dat[:,1])
+ax1.semilogx(dat[:,0], freq/(math.sqrt(2)*1.0*0.5), 'ro', markersize=10)
+ax1.set_xlabel(r'$k_{\perp}^2$')
+datEx = loadtxt('KAW-beta-10-rates.txt')
+ax1.semilogx(datEx[:,0], datEx[:,1], 'r-', label='Exact')
+ax1.set_ylabel('Normalized Frequency ($\Omega/\sqrt{2} k_{\parallel}$)')
+
+#ax2 = ax1.twinx()
+#ax2.plot(dat[:,0], 0.5*dat[:,2]/(math.sqrt(2)*1.0*0.5), 'go', markersize=10)
+#ax2.plot(datEx[:,0], -datEx[:,2], 'g-', label='Exact')
+#ax2.set_ylabel('Normalized Damping ($\gamma/\sqrt{2} k_{\parallel}$)', color='g')
+title(r'$\hat{\beta}=10$')
+
+pylab.axis('tight')
+savefig('freq-damp-shear-alf-beta-10-kp-scan-i.png', bbox_inches='tight')
+show()
+close()
