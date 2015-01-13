c1cc687acddc8a3c8328244641d2a4da4b7efc55
Ammar <11265732+ammarhakim@users.noreply.github.com>
2015-01-12 21:20:10 -0800

    Using J. Ng file instead

--- sims/s426/s426-is-coal.lua
+++ sims/s426/s426-is-coal.lua
@@ -1,4 +1,4 @@
--- Island coalescence problem
+-- GEM-challenge problem
 
 Pi = Lucee.Pi
 log = Lucee.logInfo
@@ -7,50 +7,46 @@ log = Lucee.logInfo
 gasGamma = 5./3.
 elcCharge = -1.0
 ionCharge = 1.0
-ionMass = 1.0
-elcMass = ionMass/25
+ionMass = 25.0
+elcMass = 1.0
+lightSpeed = 1.0
 epsilon0 = 1.0
-mu0 = 1.0
-lightSpeed = 1/math.sqrt(epsilon0*mu0)
 mgnErrorSpeedFactor = 1.0
 
-n0 = 1.0
-wpe = math.sqrt(n0*elcCharge^2/(epsilon0*elcMass))
-wpi = math.sqrt(n0*ionCharge^2/(epsilon0*ionMass))
-di = lightSpeed/wpi
-
-lambda = 5.0*di
+lambda = 5.0*math.sqrt(ionMass)
 Lx = 4*Pi*lambda
 Ly = Lx/2
+n0 = 1.0
 Valf = 0.1
 plasmaBeta = 1.0
 TiOverTe = 1.0
 nbOverN0 = 0.2
 pert = 0.1
 islandWidth = 0.4
-vte = lightSpeed*0.35
-T0 = vte^2*elcMass/2
 
-B0 = Valf*math.sqrt(mu0*n0*ionMass)
-OmegaCe0 = elcCharge*B0/elcMass
+B0 = Valf*math.sqrt(n0*ionMass)
 OmegaCi0 = ionCharge*B0/ionMass
 psi0 = pert*B0
 
 -- resolution and time-stepping
 NX = 640
 NY = 320
-cfl = 0.9
+cfl = 0.45
 tStart = 0.0
 tEnd = 2.5*Lx/Valf
-nFrames = 50
+nFrames = 150
 
-log(string.format("di=%g", di))
-log(string.format("wpe/OmegaCe=%g", -wpe/OmegaCe0))
-log(string.format("Lx=%gdi", Lx/di))
+log(string.format("elcMass/ionMass=1/%g",ionMass/elcMass))
+log(string.format("Lx=%gdi=%gde", Lx,Lx*math.sqrt(ionMass/elcMass)))
 log(string.format("plasmaBeta=%g", plasmaBeta))
-log(string.format("Valf/c=%g", Valf/lightSpeed))
-log(string.format("Vthe/c=%g", math.sqrt(2*T0/elcMass)/lightSpeed))
+log(string.format("Valf/c=%g", Valf))
+log(string.format("lambda/di=%g", lambda))
+log(string.format("TiOverTe=%g", TiOverTe))
+log(string.format("nbOverN0=%g", nbOverN0))
+log(string.format("pert=%g", pert))
 log(string.format("tEnd=%g,  nFrames=%d",tEnd,nFrames))
+log(string.format("NX=%d,dx=%gdi=%gde,cfl=%g\n", NX,Lx/NX,Lx/NX*math.sqrt(ionMass/elcMass),cfl))
+
 
 ------------------------------------------------
 -- COMPUTATIONAL DOMAIN, DATA STRUCTURE, ETC. --
