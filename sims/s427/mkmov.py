a38e715797beb47bfabdb14ebbb2d5d5ac6e2b75
Ammar <11265732+ammarhakim@users.noreply.github.com>
2015-01-14 13:37:56 -0500

    Added new movie script

---
+++ sims/s427/mkmov.py
@@ -0,0 +1,38 @@
+from pylab import *
+import tables
+import vistools
+
+
+def getMeshGrid(grid):
+    xl, yl = grid._v_attrs.vsLowerBounds
+    xu, yu = grid._v_attrs.vsUpperBounds
+    nx, ny = grid._v_attrs.vsNumCells
+    dx = (xu-xl)/nx
+    dy = (yu-yl)/ny
+    X = linspace(xl+0.5*dx, xu-0.5*dx, nx)
+    Y = linspace(yl+0.5*dy, yu-0.5*dy, ny)
+
+    return meshgrid(X, Y)
+
+def mkFig(fh, XX, YY, dat, nm):
+    tm = fh.root.timeData._v_attrs.vsTime
+    Valf = 0.1
+    Lx = 4*pi*5.0
+    tmAlf = tm/(Lx/Valf)
+    
+    f = figure(1)
+    pcolormesh(XX, YY, dat.transpose())
+    axis('image')
+    colorbar()
+    title("T = %.4g" % tmAlf)
+    
+    savefig(nm)
+    close()
+
+for i in range(51):
+    print ("Working on %d .." % i)
+    fh = tables.openFile("../s427/s427-is-coal_q_%d.h5" % i)
+    q = fh.root.StructGridField
+    X, Y = getMeshGrid(fh.root.StructGrid)
+    mkFig(fh, X, Y, q[:,:,3], 's427-Jze-%05d.png' % i)
+    fh.close()
