620245da7c592d72ae26c49328f15c974807b941
Ammar <11265732+ammarhakim@users.noreply.github.com>
2015-01-14 02:05:18 -0500

    Updates to plotting script.

--- sims/s427/mkplots.py
+++ sims/s427/mkplots.py
@@ -1,10 +1,6 @@
 from pylab import *
 import tables
-
-import pylab
-import numpy
-from matplotlib import rcParams
-import matplotlib.pyplot as plt
+import vistools
 
 
 def getMeshGrid(grid):
@@ -24,18 +20,6 @@ def getXYMid(grid):
 
     return 0.5*(xl+xu), 0.5*(yl+yu)
 
-# read s426 data
-fh = tables.openFile("../s426/s426-is-coal_q_25.h5")
-q26 = fh.root.StructGridField
-X26, Y26 = getMeshGrid(fh.root.StructGrid)
-
-xmid, ymid = getXYMid(fh.root.StructGrid)
-
-# read s427 data
-fh = tables.openFile("../s427/s427-is-coal_q_25.h5")
-q27 = fh.root.StructGridField
-X27, Y27 = getMeshGrid(fh.root.StructGrid)
-
 def mkFig(XX, YY, dat, nm):
     f = figure(1)
     pcolormesh(XX, YY, dat.transpose())
@@ -43,10 +27,21 @@ def mkFig(XX, YY, dat, nm):
     gca().set_xlim([-10,10])
     gca().set_ylim([-5,5])
     colorbar()
+    clim(-0.012, 0.009)
+    
     savefig(nm)
     close()
 
+fh = tables.openFile("../s426/s426-is-coal_q_25.h5")
+q26 = fh.root.StructGridField
+X26, Y26 = getMeshGrid(fh.root.StructGrid)
 mkFig(X26, Y26, q26[:,:,3], 's426-Jze-cmp.png')
+
+fh = tables.openFile("../s427/s427-is-coal_q_25.h5")
+q27 = fh.root.StructGridField
+X27, Y27 = getMeshGrid(fh.root.StructGrid)
 mkFig(X27, Y27, q27[:,:,3], 's427-Jze-cmp.png')
+
+show()
 
 
