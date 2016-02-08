9afb642827fdf4479cce967dadaf764cb1cb404d
Ammar Hakim <a.hakim777@gmail.com>
2016-02-07 18:06:33 -0800

    More changes to plotting

--- sims-2/es-shock-1d/f3/plot-cmp.sh
+++ sims-2/es-shock-1d/f3/plot-cmp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+python ../code/cmp-5m-vlasov.py -k ../m3/m3-es-shock -f f3-5m-es-shock -o f3-m3 -m 1836.2 -n 50
 python ../code/cmp-5m-vlasov.py -k ../m3/m3-es-shock -f f3-5m-es-shock -o f3-m3 -m 1836.2 -n 100
--- sims-2/es-shock-1d/f5/plot-cmp.sh
+++ sims-2/es-shock-1d/f5/plot-cmp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+python ../code/cmp-5m-vlasov.py -k ../m5/m5-es-shock -f f5-5m-es-shock -o f5-m5 -m 1836.2 -n 50
 python ../code/cmp-5m-vlasov.py -k ../m5/m5-es-shock -f f5-5m-es-shock -o f5-m5 -m 1836.2 -n 100
