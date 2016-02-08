08b8315bf3c1737645ad43ba90b7cee65ea30dd9
Ammar <11265732+ammarhakim@users.noreply.github.com>
2016-02-07 18:42:37 -0800

    More sims

---
+++ sims-2/es-shock-1d/f8/plot-cmp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python ../code/cmp-5m-vlasov.py -k ../m8/m8-es-shock -f f8-5m-es-shock -o f8-m8 -m 1836.2 -n 50
+python ../code/cmp-5m-vlasov.py -k ../m8/m8-es-shock -f f8-5m-es-shock -o f8-m8 -m 1836.2 -n 100
