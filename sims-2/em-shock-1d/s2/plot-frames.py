2b49d89cc47a12e16cba16ae4b6ef3e9cde969e5
Ammar Hakim <a.hakim777@gmail.com>
2016-09-07 23:01:40 -0700

    Plotting update

--- sims-2/em-shock-1d/s2/plot-frames.py
+++ sims-2/em-shock-1d/s2/plot-frames.py
@@ -5,7 +5,7 @@ import gkedgbasis
 for i in range(0,51):
     print("Working on %d ..." % i)
     d = gkedata.GkeData("s2-em-shock_distfElc_%d.h5" % i)
-    dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder1Basis(d)
+    dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder2Basis(d)
     X, Y, Z, fv = dg.project(0)
     nx, nvx, nvy = X.shape[0], X.shape[1], X.shape[2]
     figure(1)
@@ -24,7 +24,7 @@ for i in range(0,51):
 for i in range(0,51):
     print("Working on %d ..." % i)
     d = gkedata.GkeData("s2-em-shock_distfIon_%d.h5" % i)
-    dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder1Basis(d)
+    dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder2Basis(d)
     X, Y, Z, fv = dg.project(0)
     nx, nvx, nvy = X.shape[0], X.shape[1], X.shape[2]
     figure(1)
