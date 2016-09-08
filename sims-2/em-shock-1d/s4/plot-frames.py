d78302abb89ed5cd9399d3d841e397461d324388
Ammar <11265732+ammarhakim@users.noreply.github.com>
2016-09-07 23:34:11 -0700

    Added plot

--- sims-2/em-shock-1d/s4/plot-frames.py
+++ sims-2/em-shock-1d/s4/plot-frames.py
@@ -2,26 +2,40 @@ from pylab import *
 import gkedata
 import gkedgbasis
 
-for i in range(0,41):
+for i in range(0,51):
     print("Working on %d ..." % i)
     d = gkedata.GkeData("s4-em-shock_distfElc_%d.h5" % i)
     dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder2Basis(d)
     X, Y, Z, fv = dg.project(0)
-    pcolormesh(transpose(fv[:,24,:]))
+    nx, nvx, nvy = X.shape[0], X.shape[1], X.shape[2]
+    figure(1)
+    subplot(2,1,1)
+    pcolormesh(transpose(fv[:,:,nvy/2]))
     axis('tight')
     colorbar()
+    subplot(2,1,2)
+    pcolormesh(transpose(fv[:,nvx/2,:]))
+    axis('tight')
+    colorbar()    
     savefig('s4-em-shock_distfElc_X_VX_%05d.png' % i)
     close()
     d.close()
 
-for i in range(0,41):
+for i in range(0,51):
     print("Working on %d ..." % i)
     d = gkedata.GkeData("s4-em-shock_distfIon_%d.h5" % i)
     dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder2Basis(d)
     X, Y, Z, fv = dg.project(0)
-    pcolormesh(transpose(fv[:,24,:]))
+    nx, nvx, nvy = X.shape[0], X.shape[1], X.shape[2]
+    figure(1)
+    subplot(2,1,1)
+    pcolormesh(transpose(fv[:,:,nvy/2]))
     axis('tight')
     colorbar()
+    subplot(2,1,2)
+    pcolormesh(transpose(fv[:,nvx/2,:]))
+    axis('tight')
+    colorbar()    
     savefig('s4-em-shock_distfIon_X_VX_%05d.png' % i)
     close()
     d.close()    
