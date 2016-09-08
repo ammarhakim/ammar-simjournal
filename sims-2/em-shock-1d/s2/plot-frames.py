0aaad4c885a3012db4c9ff2e5ec9e611d660e215
Ammar <11265732+ammarhakim@users.noreply.github.com>
2016-09-07 22:27:20 -0700

    Added more plotting stuff

--- sims-2/em-shock-1d/s2/plot-frames.py
+++ sims-2/em-shock-1d/s2/plot-frames.py
@@ -4,24 +4,24 @@ import gkedgbasis
 
 for i in range(0,41):
     print("Working on %d ..." % i)
-    d = gkedata.GkeData("s4-em-shock_distfElc_%d.h5" % i)
+    d = gkedata.GkeData("s2-em-shock_distfElc_%d.h5" % i)
     dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder2Basis(d)
     X, Y, Z, fv = dg.project(0)
     pcolormesh(transpose(fv[:,24,:]))
     axis('tight')
     colorbar()
-    savefig('s4-em-shock_distfElc_X_VX_%05d.png' % i)
+    savefig('s2-em-shock_distfElc_X_VX_%05d.png' % i)
     close()
     d.close()
 
 for i in range(0,41):
     print("Working on %d ..." % i)
-    d = gkedata.GkeData("s4-em-shock_distfIon_%d.h5" % i)
+    d = gkedata.GkeData("s2-em-shock_distfIon_%d.h5" % i)
     dg = gkedgbasis.GkeDgSerendipNorm3DPolyOrder2Basis(d)
     X, Y, Z, fv = dg.project(0)
     pcolormesh(transpose(fv[:,24,:]))
     axis('tight')
     colorbar()
-    savefig('s4-em-shock_distfIon_X_VX_%05d.png' % i)
+    savefig('s2-em-shock_distfIon_X_VX_%05d.png' % i)
     close()
     d.close()    
