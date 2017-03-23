4209610110c7660d45393478afd53bfde8e86df3
Ammar <11265732+ammarhakim@users.noreply.github.com>
2017-03-23 15:48:31 -0400

    Added new input files to study issues of positivity

---
+++ sims-2/two-stream/p1f/plot-energy.py
@@ -0,0 +1,13 @@
+from pylab import *
+
+fEnergy = loadtxt("p1f-two-stream_fieldEnergy.dat")
+pEnergy = loadtxt("p1f-two-stream_totalPtclEnergy.dat")
+
+T = fEnergy[:,0]
+tEnergy = fEnergy[:,1] + pEnergy[:,1]
+plot(T, tEnergy, '-r')
+tMax = tEnergy.max()
+#gca().set_ylim([0.99*tMax, 1.01*tMax])
+error = 100*(tEnergy[-1]-tEnergy[0])/tEnergy[0]
+title('Error %g %s' % (error, "%"))
+show()
