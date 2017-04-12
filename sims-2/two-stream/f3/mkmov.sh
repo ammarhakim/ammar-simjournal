d8339d1e2326c50c4a4a60794dff4d3c6284a557
Ammar <11265732+ammarhakim@users.noreply.github.com>
2017-04-11 21:19:01 -0400

    Seems like it works

--- sims-2/two-stream/f3/mkmov.sh
+++ sims-2/two-stream/f3/mkmov.sh
@@ -1,1 +1,1 @@
-$HOME/buildDir/ffmpeg -i f3-5m-two-stream_u_%03d.png -vcodec libx264 -pix_fmt yuv420p -crf 25 5m-two-stream.mp4
+$HOME/buildDir/ffmpeg -i f3-5m-two-stream_u_%05d.png -vcodec libx264 -pix_fmt yuv420p -crf 30 5m-two-stream.mp4
