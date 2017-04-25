2be701b6e590263474b5464308a579eea0b2c1c1
Ammar <11265732+ammarhakim@users.noreply.github.com>
2017-04-25 15:52:11 -0400

    Added new sims

---
+++ sims-2/es-iaw/s5/calcdiag.sh
@@ -0,0 +1,4 @@
+/Users/ahakim/gkylsoft/postgkyl/postgkyl/gplot.py s5-es-iaw_totalPtclEnergyElc_ --write --dont
+/Users/ahakim/gkylsoft/postgkyl/postgkyl/gplot.py s5-es-iaw_totalPtclEnergyIon_ --write --dont
+/Users/ahakim/gkylsoft/postgkyl/postgkyl/gplot.py s5-es-iaw_emEnergy_ --write --dont
+/Users/ahakim/gkylsoft/postgkyl/postgkyl/gplot.py s5-es-iaw_phiInCell_ --write --dont
