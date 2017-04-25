3404572a1e448811f49f304109df3a1c4ac833cd
ammarhakim <a.hakim777@gmail.com>
2017-04-24 23:33:07 -0400

    Added ES IAW sims

---
+++ sims-2/es-iaw/s1/plot-energy.py
@@ -0,0 +1,28 @@
+from pylab import *
+
+import pylab as plt
+plt.style.use('../paper.mplstyle')
+
+emDat = loadtxt("s1-es-iaw_emEnergy.dat")
+ionDat = loadtxt("s1-es-iaw_totalPtclEnergyIon.dat")
+elcDat = loadtxt("s1-es-iaw_totalPtclEnergyElc.dat")
+
+T = emDat[:,0]
+
+figure(1)
+plot(T, emDat[:,1], 'r-')
+title('ES Energy')
+
+figure(2)
+plot(T, ionDat[:,1], 'r-')
+title('Ion particle Energy')
+
+figure(3)
+plot(T, elcDat[:,1], 'r-')
+title('Electron particle Energy')
+
+figure(4)
+eTot = emDat[:,1]+ionDat[:,1]+elcDat[:,1]
+plot(T, eTot, 'r-')
+
+show()
