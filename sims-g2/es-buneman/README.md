df86d89dabd860c2fa8f652443f3419c6beaddd0
ammarhakim <a.hakim777@gmail.com>
2018-05-17 22:27:09 -0400

    Added Buneman input files

---
+++ sims-g2/es-buneman/README.md
@@ -0,0 +1,11 @@
+EM simulations of the ES Buneman instability. In these simulations a
+"ghost" current is added to ensure that the plasma does not develop a
+inductive electric field to cancel out the electron current.
+
+All simulations use p=2 Serendipity basis.
+
+- b1: Mass ratio 25
+- b2: Mass ratio 50
+- b3: Mass ratio 100
+- b4: Mass ratio 200
+- b5: Mass ratio 400
---
+++ sims-g2/es-buneman/coda/plot-growth.py
@@ -0,0 +1,20 @@
+from pylab import *
+style.use('postgkyl.mplstyle')
+
+M = linspace(20, 420, 100)
+g = sqrt(3.0)/2.0*(0.5/M)**(1/3)*(1-0.5*(0.5/M)**(1/3))
+
+dat = loadtxt("growth-rates.txt")
+
+figure(1)
+
+plot(M, g, '-k', label=r'Max $\gamma$')
+plot(dat[:,0], dat[:,1], 'ro', label=r'G2  $\gamma$')
+grid()
+xlabel(r'Mass Ratio $m_i/m_e$')
+ylabel(r'Growth Rate $\gamma/\omega_{pe}$')
+title('Buneman Instability')
+legend(loc='best')
+
+savefig('buneman-growth-cmp.png', dpi=150)
+show()
