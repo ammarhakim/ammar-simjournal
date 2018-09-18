e1d54eacd64287186a803181b4a71e77e83d4bfd
Ammar <11265732+ammarhakim@users.noreply.github.com>
2018-09-18 15:03:23 -0400

    Small fixes to random perturbations

--- sims-g2/gk-nstx/c2/c2-Lz4-lbo-collisions.lua
+++ sims-g2/gk-nstx/c2/c2-Lz4-lbo-collisions.lua
@@ -102,7 +102,7 @@ plasmaApp = Plasma.App {
                  local effectiveSource = sourceDensity(t,{x,y,0})
                  local c_ss = math.sqrt(5/3*sourceTemperature(t,{x,y,0})/mi)
                  local nPeak = 4*math.sqrt(5)/3/c_ss*Ls*effectiveSource/2
-                 local perturb = 1e-3*math.random(-1,1)
+                 local perturb = 1e-3*(math.random()-0.5)*2.0
                  if math.abs(z) <= Ls then
                     return nPeak*(1+math.sqrt(1-(z/Ls)^2))/2*(1+perturb)
                  else
@@ -144,7 +144,7 @@ plasmaApp = Plasma.App {
                  local effectiveSource = sourceDensity(t,{x,y,0})
                  local c_ss = math.sqrt(5/3*sourceTemperature(t,{x,y,0})/mi)
                  local nPeak = 4*math.sqrt(5)/3/c_ss*Ls*effectiveSource/2
-                 local perturb = 1e-3*math.random(-1,1)
+                 local perturb = 1e-3*(math.random()-0.5)*2.0
                  if math.abs(z) <= Ls then
                     return nPeak*(1+math.sqrt(1-(z/Ls)^2))/2*(1+perturb)
                  else
