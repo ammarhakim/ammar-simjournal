9b1c1d0a9aa188664d53feaf40e0a732827024c1
Ammar <11265732+ammarhakim@users.noreply.github.com>
2019-03-04 22:45:11 -0500

    Script to compute KE spectra and not density

--- sims-g2/euler-kh/s3/mkmov.sh
+++ sims-g2/euler-kh/s3/mkmov.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..1}
+for i in {0..200}
 do
     fName=`printf "s3-euler-kh_fluid_%05d.png" $i`
     cmd="pgkyl -f s3-euler-kh_fluid_$i.bp sel -c0 pl --fix-aspect --no-show --saveas $fName"
