f967653ea2b0469467e503fc3effecde8e2bd7a1
ammarhakim <a.hakim777@gmail.com>
2019-03-05 21:45:01 -0500

    Scripts to calculate ke(k)

---
+++ sims-g2/euler-kh/s5/mkmov.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {0..200}
+do
+    fName=`printf "s5-euler-kh_fluid_%05d.png" $i`
+    cmd="pgkyl -f s5-euler-kh_fluid_$i.bp sel -c0 pl --fix-aspect --no-show --saveas $fName"
+    echo $cmd
+    $cmd
+done
