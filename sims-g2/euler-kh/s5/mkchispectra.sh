f43238d4c92b593e49e0e554084eea81cf384086
Ammar <11265732+ammarhakim@users.noreply.github.com>
2019-03-07 10:17:17 -0500

    To compute enstrophy spectra

---
+++ sims-g2/euler-kh/s5/mkchispectra.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {190..200}
+do
+    fName=`printf "s5-euler-kh_chi-spect_%05d.bp" $i`
+    cmd="pgkyl -f s5-euler-kh_fluid_$i.bp euler -v vel -g 1.4 sel -c0:2 ev 'f0 curl sq' integrate 0 fft -p write -f $fName"
+    echo $cmd
+    $cmd
+done
