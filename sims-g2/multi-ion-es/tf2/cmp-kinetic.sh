0d9c82d82d110c52cf9a6e81c4f8a78299f5bd2c
Ammar <11265732+ammarhakim@users.noreply.github.com>
2019-04-20 17:38:55 -0400

    Added shell script to run pgkyl to make comparison plots

---
+++ sims-g2/multi-ion-es/tf2/cmp-kinetic.sh
@@ -0,0 +1,1 @@
+pgkyl -f tf2-shock_ion_10.bp -l "M" -f ../t2/t2-lbo-shock_ion_M0_10.bp dataset -i1 interp -p 2 -b ms ev -l "K" "f0 1836.2 *" dataset -i0,2 sel -c0 --c0 15.0:35.0 pl -f0 --xlabel "x" --ylabel "Number Density" --saveas "fluid-kin-cmp-ion.png"
