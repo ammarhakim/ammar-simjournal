426f4108384f93dc904a05001326600522f3b775
Ammar <11265732+ammarhakim@users.noreply.github.com>
2019-04-21 22:54:08 -0400

    - Adding new set of simulations to test recovery based discontinous   difference method - Some updates to multi-ion shock problems

--- sims-g2/multi-ion-es/tf2/cmp-kinetic.sh
+++ sims-g2/multi-ion-es/tf2/cmp-kinetic.sh
@@ -1,1 +1,3 @@
-pgkyl -f tf2-shock_ion_10.bp -l "M" -f ../t2/t2-lbo-shock_ion_M0_10.bp dataset -i1 interp -p 2 -b ms ev -l "K" "f0 1836.2 *" dataset -i0,2 sel -c0 --c0 15.0:35.0 pl -f0 --xlabel "x" --ylabel "Density" --saveas "fluid-kin-cmp-ion.png"
+pgkyl -f tf2-shock_ion_10.bp -l "M" -f ../t2/t2-lbo-shock_ion_M0_10.bp dataset -i1 interp -p 2 -b ms ev -l "K" "f0 1836.2 *" dataset -i0,2 sel -c0 --c0 15.0:35.0 pl -f0 --xlabel "x" --ylabel "Density" --saveas "fluid-kin-cmp-ion.png" &
+
+pgkyl -f tf2-shock_ion_50.bp -l "M" -f ../t2/t2-lbo-shock_ion_M0_50.bp dataset -i1 interp -p 2 -b ms ev -l "K" "f0 1836.2 *" dataset -i0,2 sel -c0 --c0 10.0:35.0 pl -f0 --xlabel "x" --ylabel "Density" --saveas "fluid-kin-cmp-ion.png" &
