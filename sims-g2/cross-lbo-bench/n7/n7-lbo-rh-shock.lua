9ae7503834f53365082ee06b9fd8eebf7429162c
Ammar <11265732+ammarhakim@users.noreply.github.com>
2019-05-13 12:47:36 -0400

    Updated input files to make the tests more careful. See tiny differences

--- sims-g2/cross-lbo-bench/n7/n7-lbo-rh-shock.lua
+++ sims-g2/cross-lbo-bench/n7/n7-lbo-rh-shock.lua
@@ -77,7 +77,7 @@ plasmaApp = Plasma.App {
       evolveCollisions = true,
       -- collisions
       lbo = Plasma.LBOCollisions {
-	 collideWith = {"neut1"},
+	 collideWith = {"neut2"},
 	 frequencies = {nu},
       },
 
@@ -124,7 +124,7 @@ plasmaApp = Plasma.App {
 
       -- collisions
       lbo = Plasma.LBOCollisions {
-	 collideWith = {"neut2"},
+	 collideWith = {"neut1"},
 	 frequencies = {nu},
       },      
 
