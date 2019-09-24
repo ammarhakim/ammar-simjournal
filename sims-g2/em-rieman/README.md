108886974329f105ac71565ea00b03c5a756b74c
ammarhakim <a.hakim777@gmail.com>
2019-09-24 13:13:59 -0400

    EM Riemann problem input files

---
+++ sims-g2/em-rieman/README.md
@@ -0,0 +1,12 @@
+Multi-fluid and kinetic simulations of EM Riemann problem. This is
+repeating the classic Riemann problem to put into a paper as a
+benchmark and to study the details of the waves and, with the kinetic
+code, the various damping mechanism present. The domain size is also
+much bigger as it seems that the boundaries are impacting the
+solution.
+
+- t1: 5m, q=1; 2000 cells on [-2,2] domain
+- t2: 5m, q=10; 8000 cells ... 
+- t3: 5m, q=100; 8000 cells ...
+- t4: 5m, q=1000; 12000 cells ..
+- t5: 5m, q=1000; 4000 cells on [-1/2, 1/2]
---
+++ sims-g2/em-rieman/t1/t1-5m-em-shock.lua
@@ -0,0 +1,81 @@
+-- Gkyl ------------------------------------------------------------------------
+local Moments = require("App.PlasmaOnCartGrid").Moments
+local Euler = require "Eq.Euler"
+
+charge = 1.0
+gasGamma = 5.0/3.0
+elcCharge = -charge
+elcMass = 1/1836.2
+ionCharge = charge
+ionMass = 1.0
+
+momentApp = Moments.App {
+   logToFile = true,
+
+   tEnd = 10.0,
+   nFrame = 10,
+   lower = {-2.0},
+   upper = {2.0},
+   cells = {2000},
+   timeStepper = "fvDimSplit",
+
+   -- electrons
+   elc = Moments.Species {
+      charge = elcCharge, mass = elcMass,
+      equation = Euler { gasGamma = gasGamma },
+      equationInv = Euler { gasGamma = gasGamma, numericalFlux = "lax" },
+      forceInv = false,
+      -- initial conditions
+      init = function (t, xn)
+	 local x, sloc = xn[1], 0.0
+	 if (x<sloc) then
+	    return 1.0*elcMass/ionMass, 0.0, 0.0, 0.0, 5.0e-5/(gasGamma-1)
+	 else
+	    return 0.125*elcMass/ionMass, 0.0, 0.0, 0.0, 5.0e-6/(gasGamma-1)
+	 end
+      end,
+      evolve = true, -- evolve species?
+      bcx = { Moments.Species.bcCopy, Moments.Species.bcCopy },
+   },
+
+   -- ions
+   ion = Moments.Species {
+      charge = ionCharge, mass = ionMass,
+      equation = Euler { gasGamma = gasGamma },
+      equationInv = Euler { gasGamma = gasGamma, numericalFlux = "lax" },
+      forceInv = false,
+      -- initial conditions
+      init = function (t, xn)
+	 local x, sloc = xn[1], 0.0
+	 if (x<sloc) then
+	    return 1.0, 0.0, 0.0, 0.0, 5.0e-5/(gasGamma-1)
+	 else
+	    return 0.125, 0.0, 0.0, 0.0, 5.0e-6/(gasGamma-1)
+	 end
+      end,
+      evolve = true, -- evolve species?
+      bcx = { Moments.Species.bcCopy, Moments.Species.bcCopy },
+   },
+
+   field = Moments.Field {
+      epsilon0 = 1.0, mu0 = 1.0,
+      init = function (t, xn)
+	 local x, sloc = xn[1], 0.0
+	 if (x<sloc) then
+	    return 0.0, 0.0, 0.0, 0.75e-2, 0.0, 1.0e-2
+	 else
+	    return 0.0, 0.0, 0.0, 0.75e-2, 0.0, -1.0e-2
+	 end
+      end,
+      evolve = true, -- evolve field?
+      bcx = { Moments.Field.bcCopy, Moments.Field.bcCopy },
+   },
+
+   emSource = Moments.CollisionlessEmSource {
+      species = {"elc", "ion"},
+      timeStepper = "direct",
+   },   
+
+}
+-- run application
+momentApp:run()
