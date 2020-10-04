fa95f4fb0e202c6c17126d29595b09a8b4f13f39
ammarhakim <a.hakim777@gmail.com>
2020-10-04 09:41:52 -0400

    More tests.

--- sims-g2/iter-poisson/README.md
+++ sims-g2/iter-poisson/README.md
@@ -10,6 +10,8 @@ STS RKL2 Tests
 Each directory contains two runs, with and without extrapolation.
 
 - s1: 32x32, RKL2 tests
+- s2: 64x64, RKL2 tests
+- s3: 128x128, RKL2 tests
 
 STS RKL1 Tests
 --------------
---
+++ sims-g2/iter-poisson/m3/m3-iter-poisson.lua
@@ -0,0 +1,25 @@
+-- load app code
+local App = dofile("../code/sts-diffusion.lua")
+
+diffusion = App {
+   polyOrder = 1,
+   lower = {-1.0, -1.0},
+   upper = {1.0, 1.0},
+   cells = {128, 128},
+   errEps = 1e-8,
+   factor = 1000,
+   extraStages = 9,
+   stepper = 'RKL1',
+  
+   -- initial conditions
+   init = function (t, xn)
+      local x, y = xn[1], xn[2]
+      return 0.0
+   end,
+
+   source = function (t, xn)
+      local x, y = xn[1], xn[2]
+      return math.exp(-10*(x^2+y^2))
+   end,
+}
+diffusion()
