20a1f330a8af1867f3f965971347f6defc5a4477
ammarhakim <a.hakim777@gmail.com>
2020-10-04 22:14:26 -0400

    RKL1 tests

---
+++ sims-g2/iter-poisson/m4/m4-iter-poisson-extra.lua
@@ -0,0 +1,27 @@
+-- load app code
+local App = dofile("../code/sts-diffusion.lua")
+
+diffusion = App {
+   polyOrder = 2,
+   lower = {-1.0, -1.0},
+   upper = {1.0, 1.0},
+   cells = {32, 32},
+   errEps = 1e-8,
+   cflFrac = 0.85,
+   factor = 120,
+   extraStages = 3,
+   stepper = 'RKL1',
+   extrapolateInterval = 2,
+   
+   -- initial conditions
+   init = function (t, xn)
+      local x, y = xn[1], xn[2]
+      return 0.0
+   end,
+
+   source = function (t, xn)
+      local x, y = xn[1], xn[2]
+      return math.exp(-10*(x^2+y^2))
+   end,
+}
+diffusion()
