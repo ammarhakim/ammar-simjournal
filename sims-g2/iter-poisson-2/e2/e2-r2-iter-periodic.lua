88fb5ff63528fc205e8a4763c5b1d09d6cf2ab6f
ammarhakim <a.hakim777@gmail.com>
2020-11-10 12:34:12 -0500

    3D convergence tests

---
+++ sims-g2/iter-poisson-2/e2/e2-r2-iter-periodic.lua
@@ -0,0 +1,113 @@
+-- Gkyl ------------------------------------------------------------------------
+--
+--
+--------------------------------------------------------------------------------
+
+local Basis = require "Basis"
+local DataStruct = require "DataStruct"
+local Grid = require "Grid"
+local Updater = require "Updater"
+local Time = require "Lib.Time"
+
+local polyOrder = 1
+local lower = {0, 0, 0}
+local upper = {2*math.pi, 2*math.pi, 2*math.pi}
+local cells = {16, 16, 16}
+local periodicDirs = {1, 2, 3}
+
+local grid = Grid.RectCart {
+   lower = lower,
+   upper = upper,
+   cells = cells,
+   periodicDirs = periodicDirs,
+}
+local basis = Basis.CartModalSerendipity {
+   ndim = grid:ndim(),
+   polyOrder = polyOrder
+}
+
+local function getField()
+   return DataStruct.Field {
+      onGrid = grid,
+      numComponents = basis:numBasis(),
+      ghost = {1, 1},
+      metaData = {
+	 polyOrder = basis:polyOrder(),
+	 basisType = basis:id(),
+      },
+   }
+end
+local fIn = getField()
+local fOut = getField()
+local fExact = getField()
+
+-- Initial conditions from
+-- http://ammar-hakim.org/sj/je/je11/je11-fem-poisson.html#convergence-of-2d-solver
+local initSource = Updater.ProjectOnBasis {
+   onGrid = grid,
+   basis = basis,
+   evaluate = function(t, xn)
+      local x, y, z = xn[1], xn[2], xn[3]
+      local amn = {{0,10,0}, {10,0,0}, {10,0,0}}
+      local bmn = {{0,10,0}, {10,0,0}, {10,0,0}}
+      local t1, t2 = 0.0, 0.0
+      local f = 0.0
+      for m = 0,2 do
+	 for n = 0,2 do
+	    t1 = amn[m+1][n+1]*math.cos(m*x)*math.cos(n*y)*math.sin(3*z)
+	    t2 = bmn[m+1][n+1]*math.sin(m*x)*math.sin(n*y)*math.sin(3*z)
+	    f = f + -(m*m+n*n+9)*(t1+t2)
+	 end
+      end
+      return -f/50.0
+   end,
+}
+
+local exactSol = Updater.ProjectOnBasis {
+   onGrid = grid,
+   basis = basis,
+   evaluate = function(t, xn)
+      local x, y, z = xn[1], xn[2], xn[3]
+      local amn = {{0,10,0}, {10,0,0}, {10,0,0}}
+      local bmn = {{0,10,0}, {10,0,0}, {10,0,0}}
+      local t1, t2 = 0.0, 0.0
+      local f = 0.0
+
+      for m = 0,2 do
+	 for n = 0,2 do
+	    t1 = amn[m+1][n+1]*math.cos(m*x)*math.cos(n*y)*math.sin(3*z)
+	    t2 = bmn[m+1][n+1]*math.sin(m*x)*math.sin(n*y)*math.sin(3*z)
+	    f = f + (t1+t2)
+	 end
+      end
+      return f/50.0
+   end,
+}
+
+local iterPoisson = Updater.IterPoisson {
+   onGrid = grid,
+   basis = basis,
+
+   -- there parameters will eventually be replaced by internal
+   -- heuristics
+   
+   errEps = 1e-8, -- maximum residual error
+   cflFrac = 8.0, -- CFL frac for internal iterations
+   stepper = 'richard2', -- stepper to use 'RKL1' or 'RKL2'
+   verbose = true,
+}
+
+initSource:advance(0.0, {}, {fIn})
+
+local tmStart = Time.clock()
+iterPoisson:advance(0.0, {fIn}, {fOut})
+print(string.format("Simulation took %g", Time.clock()-tmStart))
+
+fIn:write('fIn.bp', 0.0, 0)
+fOut:write('fOut.bp', 0.0, 0)
+
+exactSol:advance(0.0, {}, {fExact})
+fExact:write("fExact.bp")
+
+iterPoisson:writeDiagnostics()
+print(string.format("L2 error %g\n", iterPoisson:l2diff(fOut, fExact)))
